9ad108e782c040c905a1fc9dc79ca9cec4ef195f
Anderson Alves <anderson.a.roberto@gmail.com>
2025-02-19 19:30:46 -0300

    chore: update to Expo SDK 50 and configure Metro bundler

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+// Learn more https://docs.expo.dev/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/**
+ * Metro configuration for Expo projects.
+ * See: https://docs.expo.dev/guides/customizing-metro/
+ *
+ * @type {import('expo/metro-config').MetroConfig}
+ */
+const config = getDefaultConfig(__dirname);
+
+// Modify the config to support SVG files
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts.push('svg');
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+
+module.exports = config;
